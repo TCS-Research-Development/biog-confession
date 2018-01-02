ca2061b08235d3a97b5a4a7acc4acc20989930ba
Divya Pothu <divya.pothu@tcs.com>
2018-01-02 16:48:02 +0530

    theme of project

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app     = express();
+var path = require('path');
+  
+  // set the public folder to serve public assets
+  app.use(express.static(__dirname + '/'));
+  
+  // set up our one route to the index.html file
+ app.get('*', function(req, res) {
+     res.sendFile(path.join(__dirname + '/index.html'));
+ });
+ app.listen(8050);
+ console.log('port is running 8050');

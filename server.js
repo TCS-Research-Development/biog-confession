9abf868c0cfa08bdefd19bab226e849751ea1514
satish8482 <satish8482@gmail.com>
2018-01-04 19:03:13 +0530

    style for home

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 var express = require('express');
 var router = express.Router();
 
-var sequelize = new Sequelize('postgres', 'postgres', 'Divya123', {
+var sequelize = new Sequelize('postgres', 'postgres', 'Satya@9036', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres',
@@ -44,7 +44,7 @@ var sdk = new BoxSDK({
 });
  
 //Create a basic API client
-var client = sdk.getBasicClient('8ocZMeITSeGcKLCLBiGLDsMPYt9BKEih');
+var client = sdk.getBasicClient('bf9RVT4OVq86Emv1k4gCo9Ttxyk0HjaD');
  
 // Get some of that sweet, sweet data!
 client.users.get(client.CURRENT_USER_ID, null, function(err, currentUser) {
